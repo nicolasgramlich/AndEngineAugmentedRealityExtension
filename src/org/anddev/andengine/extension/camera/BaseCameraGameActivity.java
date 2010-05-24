7a695ce6fd8b210a30b5497b21755adfbb04ff60
Nicolas Gramlich (Laptop) <nicolasgramlich@gmail.com>
2010-05-24 21:49:49 +0200

    Initial import. BaseCameraGameActivity.

---
+++ src/org/anddev/andengine/extension/camera/BaseCameraGameActivity.java
@@ -0,0 +1,74 @@
+package org.anddev.andengine.extension.camera;
+
+
+import org.anddev.andengine.opengl.view.RenderSurfaceView;
+import org.anddev.andengine.opengl.view.GLSurfaceView.ComponentSizeChooser;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 21:38:32 - 24.05.2010
+ */
+public abstract class BaseCameraGameActivity extends BaseGameActivity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private CameraPreviewSurfaceView mCameraPreviewSurfaceView;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.mCameraPreviewSurfaceView = new CameraPreviewSurfaceView(this);
+		this.addContentView(this.mCameraPreviewSurfaceView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		
+		this.mRenderSurfaceView.setZOrderMediaOverlay(true);
+		this.mRenderSurfaceView.bringToFront();
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+	
+	protected void onSetContentView() {
+		this.mRenderSurfaceView = new RenderSurfaceView(this, this.mEngine);	
+
+		this.mRenderSurfaceView.setEGLConfigChooser(new ComponentSizeChooser(4, 4, 4, 4, 16, 0)); 
+		this.mRenderSurfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+
+		this.mRenderSurfaceView.applyRenderer();
+
+		this.setContentView(this.mRenderSurfaceView, createSurfaceViewLayoutParams());
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		finish();
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
